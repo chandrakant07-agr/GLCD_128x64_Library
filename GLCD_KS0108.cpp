930e3c19a8bfb4cc6235c652cd9478ef750b376e
Chandrakant Agrawal <agrawal007chandrakant@gmail.com>
2024-09-30 22:14:54 +0530

    Display direction is added, but it's not completed

--- GLCD_KS0108.cpp
+++ GLCD_KS0108.cpp
@@ -7,12 +7,6 @@ GLCD_KS0108::GLCD_KS0108(uint8_t DI, uint8_t RW, uint8_t EN,
 	init(DI, RW, EN, D0, D1, D2, D3, D4, D5, D6, D7, CS1, CS2);
 }
 
-// GLCD_KS0108::GLCD_KS0108(uint8_t DI, uint8_t RW, uint8_t EN, uint8_t D4, 
-// 		uint8_t D5, uint8_t D6, uint8_t D7, uint8_t CS1, uint8_t CS2) {
-
-// 	init(DI, RW, EN, D4, D5, D6, D7, 0, 0, 0, 0, CS1, CS2, BIT_4);
-// }
-
 void GLCD_KS0108::init(uint8_t DI, uint8_t RW, uint8_t EN, 
 		uint8_t D0, uint8_t D1, uint8_t D2, uint8_t D3, uint8_t D4, 
 		uint8_t D5, uint8_t D6, uint8_t D7, uint8_t CS1, uint8_t CS2) {
@@ -31,21 +25,16 @@ void GLCD_KS0108::init(uint8_t DI, uint8_t RW, uint8_t EN,
     dPins[5] = D5;
     dPins[6] = D6;
     dPins[7] = D7;
-
-	/* displayBitMod = bitMod;
-	dataPinsSize = displayBitMod&BIT_4
-					? (sizeof(dPins)/sizeof(uint8_t))/2
-					: sizeof(dPins)/sizeof(uint8_t); */
 					
 	dataPinsSize = sizeof(dPins)/sizeof(uint8_t);
-
+	dispDirection = Default_Top;
 }
 
 void GLCD_KS0108::begin(enum Colors color) {
-    this->lcdCoord.x = 0;
-	this->lcdCoord.y = 0;
-	this->lcdCoord.p = 0;
-    inverter = color;
+    this->virtualCoord.x = 0;
+	this->virtualCoord.y = 0;
+	this->virtualCoord.p = 0;
+	this->inverter = color;
 
     _pinDefine(rsPin, OUTPUT);
     _pinDefine(rwPin, OUTPUT);
@@ -57,11 +46,11 @@ void GLCD_KS0108::begin(enum Colors color) {
 
     _delay_ms(50);						// wait 50ms (millisecond) for LCD to power up
 
-    _pinWrite(rsPin, LOW);          	// rs or D/I(Data/Instruction) Pin
-	_pinWrite(rwPin, LOW);          	// rw(Data Read/Data Write) Pin
-	_pinWrite(enPin, LOW);          	// Enable Signal(LOW = Off, HIGH = On)
-	_pinWrite(c1Pin, HIGH);		    	// Select Chip1(LOW = Off, HIGH = On)
-	_pinWrite(c2Pin, HIGH);		    	// Select Chip2(LOW = Off, HIGH = On)
+    _pinWrite(rsPin, LOW);				// RS or D/I(Data/Instruction) Pin
+	_pinWrite(rwPin, LOW);				// RW(Data Read/Data Write) Pin
+	_pinWrite(enPin, LOW);				// Enable Signal(LOW = Off, HIGH = On)
+	_pinWrite(c1Pin, HIGH);				// Select Chip1(LOW = Off, HIGH = On)
+	_pinWrite(c2Pin, HIGH);				// Select Chip2(LOW = Off, HIGH = On)
 
     _ram_write_command(LCD_ON);			// LCD Contrast On
 	_ram_write_command(LCD_START);		// Set Start Line
@@ -71,19 +60,23 @@ void GLCD_KS0108::begin(enum Colors color) {
 }
 
 void GLCD_KS0108::gotoXY(uint8_t x, uint8_t y) {
-
-    // save new coordinates
 	this->lcdCoord.x = x;
 	this->lcdCoord.y = y;
-	this->lcdCoord.p = y/BIT_8;
+
+	directionFilter(&x, &y);
+
+    // save new coordinates
+	this->virtualCoord.x = x;
+	this->virtualCoord.y = y;
+	this->virtualCoord.p = y/BIT_8;
 
 	for(uint8_t i=0; i < DISPLAY_WIDTH/CHIP_WIDTH; i++) {
 		_select_chip(i);
-		_ram_write_command(LCD_PAGE | lcdCoord.p);
+		_ram_write_command(LCD_PAGE | virtualCoord.p);
 	}
 
-	_select_chip(lcdCoord.x/CHIP_WIDTH);
-	_ram_write_command(LCD_ADDR | lcdCoord.x % CHIP_WIDTH);
+	_select_chip(virtualCoord.x/CHIP_WIDTH);
+	_ram_write_command(LCD_ADDR | virtualCoord.x % CHIP_WIDTH);
 }
 
 void GLCD_KS0108::clearDisplay(uint8_t color) {
@@ -105,8 +98,8 @@ void GLCD_KS0108::setDisplayColor(enum Colors color) {
 void GLCD_KS0108::setPixel(uint8_t x, uint8_t y) {
 	uint8_t pageOffset, rData, pixel = BIT_1;
 
-	pageOffset = y%BIT_8;
 	gotoXY(x, y);
+	pageOffset = virtualCoord.y%BIT_8;
 	pixel <<= pageOffset;
 	rData = read_byte();
 	pixel |= rData;
@@ -128,13 +121,99 @@ uint8_t GLCD_KS0108::read_byte(void) {
 	return _ram_read_byte();		// real read
 }
 
-void GLCD_KS0108::drawHorizontalLine(uint8_t x, uint8_t y, uint8_t length) {
-	draw_line(x, y, length, 0);
+// void GLCD_KS0108::drawHorizontalLine(uint8_t x, uint8_t y, uint8_t width) {
+// 	// draw_line(x, y, length, 0);
+// 	for(uint8_t i=0; i<width; i++) setPixel(x+i, y);
+// }
+
+void GLCD_KS0108::drawVerticallyLine(uint8_t x, uint8_t y, uint8_t height) {
+	// draw_line(x, y, length, 0);
+	for(uint8_t i=0; i<height; i++) setPixel(x, y+i);
 }
 
-void GLCD_KS0108::drawVerticallyLine(uint8_t x, uint8_t y, uint8_t length) {
-	draw_line(x, y, 0, length);
+
+
+void GLCD_KS0108::drawHorizontalLine(uint8_t x, uint8_t y, uint8_t width) {
+	// draw_line(x, y, 0, length);
+	uint8_t pixel = BIT_FF, length, pageOffset, rData, color=Black;
+
+	pageOffset = x%BIT_8;
+
+	if(width < BIT_8-pageOffset) {
+		(dispDirection & BIT_8)
+			? pixel <<= (BIT_8-width)
+			: pixel >>= (BIT_8-width);
+		length = width;
+	} else {
+		length = BIT_8-pageOffset;
+	}
+
+	(dispDirection & BIT_8)
+		? pixel >>= pageOffset
+		: pixel <<= pageOffset;
+
+	while(length <= width) {
+		gotoXY(x, y);
+		rData = read_byte();
+		pixel |= rData;
+		_ram_write_data(pixel);
+		pixel = BIT_FF;
+		length += BIT_8;
+		x += BIT_8;
+	}
+
+	if(width < length && length <= DISPLAY_HEIGHT) {
+		gotoXY(x, y);
+		(dispDirection & BIT_8)
+			? pixel <<= (length-width)
+			: pixel >>= (length-width);
+		rData = read_byte();
+		pixel |= rData;
+		_ram_write_data(pixel);
+	}
 }
+
+
+
+// void GLCD_KS0108::drawVerticallyLine(uint8_t x, uint8_t y, uint8_t height) {
+// 	// draw_line(x, y, 0, length);
+// 	uint8_t pixel = BIT_FF, length, pageOffset, rData, color=Black;
+
+// 	pageOffset = y%BIT_8;
+
+// 	if(height < BIT_8-pageOffset) {
+// 		(dispDirection & BIT_4)
+// 			? pixel <<= (BIT_8-height)
+// 			: pixel >>= (BIT_8-height);
+// 		length = height;
+// 	} else {
+// 		length = BIT_8-pageOffset;
+// 	}
+
+// 	(dispDirection & BIT_4)
+// 		? pixel >>= pageOffset
+// 		: pixel <<= pageOffset;
+
+// 	while(length <= height) {
+// 		gotoXY(x, y);
+// 		rData = read_byte();
+// 		pixel |= rData;
+// 		_ram_write_data(pixel);
+// 		pixel = BIT_FF;
+// 		length += BIT_8;
+// 		y += BIT_8;
+// 	}
+
+// 	if(height < length && length <= DISPLAY_HEIGHT) {
+// 		gotoXY(x, y);
+// 		(dispDirection & BIT_4)
+// 			? pixel <<= (length-height)
+// 			: pixel >>= (length-height);
+// 		rData = read_byte();
+// 		pixel |= rData;
+// 		_ram_write_data(pixel);
+// 	}
+// }
 
 void GLCD_KS0108::drawRectangle(uint8_t x, uint8_t y, uint8_t width, uint8_t height) {
 	drawHorizontalLine(x, y, width-1);					// top
@@ -185,8 +264,61 @@ void GLCD_KS0108::drawCircle(uint8_t x, uint8_t y, uint8_t radius) {
 	drawRoundedRect(x, y, diameter, diameter, radius);
 }
 
+GLCD_KS0108& GLCD_KS0108::setDispDirection(enum Directions dir) {
+	dispDirection = dir;
+	return *this;
+}
+
 
 // mid level methods
+uint8_t GLCD_KS0108::bitsSwaper(uint8_t byte) {
+	byte = ((byte & 0xF0)>>4) | ((byte & 0x0F)<<4);
+	byte = ((byte & 0xCC)>>2) | ((byte & 0x33)<<2);
+	byte = ((byte & 0xAA)>>1) | ((byte & 0x55)<<1);
+
+	return byte;
+}
+
+void GLCD_KS0108::selectFontFamily(enum FontFamily fontType) {
+	switch(fontType) {
+		case BIT_1:
+			Font_Family = VERTICAL_FONT_8X5;
+			Font_Col = FONT_WIDTH;
+			break;
+		case BIT_2:
+			Font_Family = HORIZONTAL_FONT_8X5;
+			Font_Col = FONT_HIGHT;
+			break;
+		default:
+			break;
+	}
+}
+
+void GLCD_KS0108::directionFilter(uint8_t* x, uint8_t* y) {
+	uint8_t temp = *x;
+
+	switch(dispDirection) {
+		case BIT_1:
+			selectFontFamily(V_FONT_8X5);
+			break;
+		case BIT_2:
+			selectFontFamily(H_FONT_8X5);
+			*x = (DISPLAY_WIDTH-BIT_1)-*y;
+			*y = temp;
+			break;
+		case BIT_4:
+			selectFontFamily(V_FONT_8X5);
+			*x = (DISPLAY_WIDTH-BIT_1)-*x;
+			*y = (DISPLAY_HEIGHT-BIT_1)-*y;
+			break;
+		case BIT_8:
+			selectFontFamily(H_FONT_8X5);
+			*x = *y;
+			*y = (DISPLAY_HEIGHT-BIT_1)-temp;
+			break;
+	}
+}
+
 void GLCD_KS0108::draw_corner(uint8_t x, uint8_t y, uint8_t width, uint8_t height, 
 		uint8_t radius, uint8_t selector) {
 
@@ -214,46 +346,54 @@ void GLCD_KS0108::draw_corner(uint8_t x, uint8_t y, uint8_t width, uint8_t heigh
 }
 
 void GLCD_KS0108::draw_char(uint8_t _char) {
-	uint8_t row, pageOffset, rData, fontBit, new_fontBit;
+	uint8_t row, pageOffset, rData, fontByte, newFontByte;
 
 	// Remove leading empty characters
 	row = _char - FONT_START;			// 32 is the ASCII of the first printable character
 
-	for(uint8_t col=0; col<FONT_WIDTH; col++) {
+	for(uint8_t col=0; col<Font_Col; col++) {
+		// Find the start of the character in the font array
+		// The first byte of each line is the width/height of the character
+		fontByte = pgm_read_byte(&*(*(((uint8_t (*)[Font_Col])Font_Family) + row) + col));
+
 		// If the character exceeds screen width bounds, start the next line
 		if(lcdCoord.x >= DISPLAY_WIDTH-FONT_WIDTH && col == 0) gotoXY(0, lcdCoord.y + BIT_8);
 
-		// Find the start of the character in the font array
-		// First byte of each line is the width of the character
-		fontBit = pgm_read_byte(&*(*(FONT_8X5 + row) + col));
-
-		// Read the data from where we will start writing the font
-		gotoXY(lcdCoord.x, lcdCoord.y);
-		rData = read_byte();
+		if(dispDirection & BIT_4) {
+			fontByte = bitsSwaper(fontByte);
+		}
 
 		// set pageoffset
 		pageOffset = lcdCoord.y%BIT_8;
 
+		// Read the data from where we will start writing the font
+		// gotoXY(lcdCoord.x, lcdCoord.y);
+		rData = read_byte();
+
 		// Calculate overflowing bits related to page height
 		if(pageOffset != 0) {
-			new_fontBit = fontBit << pageOffset;
+			newFontByte = (dispDirection & BIT_4)
+							? fontByte >> pageOffset
+							: fontByte << pageOffset;
 
 			_ram_write_data(inverter
-				? ~(new_fontBit|BIT_FF>>BIT_8-pageOffset)|rData
-				: new_fontBit|rData);
+				? ~(newFontByte|BIT_FF>>BIT_8-pageOffset)|rData
+				: newFontByte|rData);
 
 			gotoXY(lcdCoord.x, lcdCoord.y + BIT_8);
 			rData = read_byte();
-			fontBit >>= BIT_8-pageOffset;
-			lcdCoord.y -= BIT_8;
+			(dispDirection & BIT_4)
+				? fontByte <<= BIT_8-pageOffset
+				: fontByte >>= BIT_8-pageOffset;
+			lcdCoord.y -= BIT_8;			// update the y-coordinate for the next line
 		}
 
 		// If pageoffset does not overflow then write Bits otherwise write remaining overflown Bits
 		_ram_write_data(inverter
-			? ~(fontBit|BIT_FF<<(pageOffset?pageOffset:BIT_8))|rData 
-			: fontBit|rData);
+			? ~(fontByte|BIT_FF<<(pageOffset?pageOffset:BIT_8))|rData
+			: fontByte|rData);
 
-		lcdCoord.x++;
+		lcdCoord.x++;					// update the x-coordinate for the next font bits
 		gotoXY(lcdCoord.x, lcdCoord.y);
 	}
 
@@ -268,53 +408,67 @@ void GLCD_KS0108::draw_char(uint8_t _char) {
 		gotoXY(lcdCoord.x, lcdCoord.y - BIT_8);
 	}
 
-	lcdCoord.x++;
+	lcdCoord.x++;			// update the x-coordinate for the next character
 }
 
-void GLCD_KS0108::draw_line(uint8_t x, uint8_t y, uint8_t width, uint8_t height) {
-	uint8_t pixel = BIT_FF, length, i, pageOffset, rData, color=Black;
 
-	pageOffset = y%BIT_8;
+// void GLCD_KS0108::draw_line(uint8_t x, uint8_t y, uint8_t width, uint8_t height) {
+// 	uint8_t pixel = BIT_FF, length, i, pageOffset, rData, color=Black;
 
-	if(height!=0) {
-		if(height < BIT_8-pageOffset) {
-			pixel >>= (BIT_8-height);
-			length = height;
-		} else {
-			length = BIT_8-pageOffset;
-		}
+// 	// gotoXY(x, y);
+// 	pageOffset = y%BIT_8;
 
-		pixel <<= pageOffset;
+// 	if(height!=0) {
+// 		if(height < BIT_8-pageOffset) {
+// 			(dispDirection & BIT_4)
+// 				? pixel <<= (BIT_8-height)
+// 				: pixel >>= (BIT_8-height);
+// 			length = height;
+// 		} else {
+// 			length = BIT_8-pageOffset;
+// 		}
 
-		while(length <= height) {
-			gotoXY(x, y);
-			rData = read_byte();
-			pixel |= rData;
-			_ram_write_data(pixel);
-			pixel = BIT_FF;
-			length += BIT_8;
-			y += BIT_8;
-		}
+// 		(dispDirection & BIT_4)
+// 			? pixel >>= pageOffset
+// 			: pixel <<= pageOffset;
+		
+// 		// pixel <<= pageOffset;
 
-		if(height < length && length <= DISPLAY_HEIGHT) {
-			gotoXY(x, y);
-			pixel >>= (length-height);
-			rData = read_byte();
-			pixel |= rData;
-			_ram_write_data(pixel);
-		}
-	} else if(width!=0) {
-		for(i=0; i<width; i++) {
-			gotoXY(x, y);
-			pixel = BIT_1 << pageOffset;
-			rData = read_byte();
-			pixel |= rData;
-			_ram_write_data(pixel);
-			x++;
-		}
-	}
-}
+// 		while(length <= height) {
+// 			gotoXY(x, y);
+// 			rData = read_byte();
+// 			pixel |= rData;
+// 			_ram_write_data(pixel);
+// 			pixel = BIT_FF;
+// 			length += BIT_8;
+// 			y += BIT_8;
+// 		}
 
+// 		if(height < length && length <= DISPLAY_HEIGHT) {
+// 			gotoXY(x, y);
+// 			(dispDirection & BIT_4)
+// 				? pixel <<= (length-height)
+// 				: pixel >>= (length-height);
+// 			rData = read_byte();
+// 			pixel |= rData;
+// 			_ram_write_data(pixel);
+// 		}
+// 	} else if(width!=0) {
+// 		for(i=0; i<width; i++) {
+// 			// gotoXY(x, y);
+// 			// pageOffset = virtualCoord.y%BIT_8;
+// 			// pixel = BIT_1 << pageOffset;
+// 			// rData = read_byte();
+// 			// pixel |= rData;
+// 			// _ram_write_data(pixel);
+// 			// x++;
+// 			setPixel(x+i, y);
+// 		}
+// 	}
+// }
+
+
+// low level methods
 void GLCD_KS0108::_select_chip(uint8_t chip) {
 	if(chip == Chip_1)
 		_pinWrite(c1Pin, HIGH);
@@ -327,8 +481,6 @@ void GLCD_KS0108::_select_chip(uint8_t chip) {
 		_pinWrite(c2Pin, LOW);
 }
 
-
-// low level methods
 uint8_t GLCD_KS0108::_ram_read_byte(void) {
 	uint8_t rData = 0;
 
@@ -350,7 +502,7 @@ uint8_t GLCD_KS0108::_ram_read_byte(void) {
 	// reset data pins in write mode
 	for(int i=0; i<dataPinsSize; i++) _pinDefine(dPins[i], OUTPUT);
 	gotoXY(lcdCoord.x, lcdCoord.y);
-	// debug = rData;
+
 	return rData;
 }
 
@@ -366,13 +518,6 @@ void GLCD_KS0108::__send_bits(uint8_t _val, uint8_t _mod) {
     // for(int i=0; i<dataPinsSize; i++) _pinDefine(dPins[i], OUTPUT);
     _pinWrite(rsPin, _mod & BIT_1);
     _pinWrite(rwPin, _mod & BIT_0);
-
-	// if(displayBitMod & BIT_4) {
-	//     __write_bits(_val>>4, BIT_4);					// Send higher nibble
-	//     __write_bits(_val & LOW_NIBBLE, BIT_4);			// Send lower nibble
-	// } else if(displayBitMod & BIT_8) {
-	//     __write_bits(_val, BIT_0);
-	// }
 
 	__write_bits(_val);
 }
--- GLCD_KS0108.h
+++ GLCD_KS0108.h
@@ -5,7 +5,7 @@
 
 #define DISPLAY_WIDTH	128
 #define DISPLAY_HEIGHT	64
-#define CHIP_WIDTH		64
+#define CHIP_WIDTH      64
 
 #define LCD_ON		0x3F
 #define LCD_OFF		0x3E
@@ -16,18 +16,24 @@
 #define BIT_0   0x00
 #define BIT_1   0x01
 #define BIT_2   0x02
+#define BIT_3   0x03
 #define BIT_4   0x04
+#define BIT_5   0x05
+#define BIT_6   0x06
 #define BIT_7   0x07
 #define BIT_8   0x08
 #define BIT_15  0x0F
 #define BIT_FF  0xFF
 
-#define LOW_NIBBLE BIT_15
+struct Virtual_Coordinates {
+    uint8_t x;
+    uint8_t y;
+    uint8_t p;
+};
 
-struct Coordinate {
-  uint8_t x;
-  uint8_t y;
-  uint8_t p;
+struct Coordinates {
+    uint8_t x;
+    uint8_t y;
 };
 
 enum Chip_Set {
@@ -39,80 +45,100 @@ enum Colors {
     Black = BIT_FF
 };
 
+enum Directions {
+    Default_Top = BIT_1,
+    Right_Top   = BIT_2,
+    Bottom_Top  = BIT_4,
+    Left_Top    = BIT_8
+};
+
+enum FontFamily {
+    V_FONT_8X5 = BIT_1,
+    H_FONT_8X5 = BIT_2
+};
+
 class GLCD_Arduino {
     private:
       /* data */
     public:
       // GLCD_Arduino(/* args */);
-      void _pinDefine(uint8_t _p_, uint8_t _mod_);
-      void _pinWrite(uint8_t _p_, uint8_t _mod_);
-      uint8_t _pinRead(uint8_t _p_);
+      void _pinDefine(uint8_t _pin, uint8_t _mod);
+      void _pinWrite(uint8_t _pin, uint8_t _mod);
+      uint8_t _pinRead(uint8_t _pin);
 };
 
 class GLCD_KS0108: public GLCD_Arduino {
-  private:
-    //   const byte chipCondition[2] = {1, 2};
-      uint8_t inverter;
-      uint8_t dPins[8];
-      uint8_t rsPin, rwPin, enPin, c1Pin, c2Pin;
-      uint8_t dataPinsSize;
-      // uint8_t displayBitMod;
+    private:
+        struct Virtual_Coordinates virtualCoord;
+        uint8_t inverter;
+        uint8_t dPins[8];
+        uint8_t rsPin, rwPin, enPin, c1Pin, c2Pin;
+        uint8_t dataPinsSize;
+        uint8_t dispDirection;
+        uint8_t Font_Col;
+        const void* Font_Family;
 
-      // low level methods
-      void _select_chip(uint8_t chip);
-      uint8_t _ram_read_byte(void);
-      void _ram_write_command(uint8_t _val);
-      void _ram_write_data(uint8_t _val);
+        // low level methods
+        void _select_chip(uint8_t chip);
+        uint8_t _ram_read_byte(void);
+        void _ram_write_command(uint8_t _val);
+        void _ram_write_data(uint8_t _val);
 
-      void __send_bits(uint8_t _val, uint8_t _mod);
-      void __write_bits(uint8_t _val);
-      void __pulse_enable(void);
-      void __delay_ns(void);
+        void __send_bits(uint8_t _val, uint8_t _mod);
+        void __write_bits(uint8_t _val);
+        void __pulse_enable(void);
+        void __delay_ns(void);
 
-      //mid level methods
-      void draw_char(uint8_t _char);
-      void draw_line(uint8_t x, uint8_t y, uint8_t width, uint8_t height);
-      void draw_corner(uint8_t x, uint8_t y, uint8_t width, uint8_t height, 
-          uint8_t radius, uint8_t selector);
+        //mid level methods
+        uint8_t bitsSwaper(uint8_t byte);
+        void selectFontFamily(enum FontFamily fontType);
+        void directionFilter(uint8_t* x, uint8_t* y);
+        void draw_char(uint8_t _char);
+        // void draw_line(uint8_t x, uint8_t y, uint8_t width, uint8_t height);
+        void draw_corner(uint8_t x, uint8_t y, uint8_t width, uint8_t height, 
+            uint8_t radius, uint8_t selector);
 
-      void init(uint8_t DI, uint8_t RW, uint8_t EN, 
-          uint8_t D0, uint8_t D1, uint8_t D2, uint8_t D3, uint8_t D4, 
-          uint8_t D5, uint8_t D6, uint8_t D7, uint8_t CS1, uint8_t CS2);
+        void init(uint8_t DI, uint8_t RW, uint8_t EN, 
+            uint8_t D0, uint8_t D1, uint8_t D2, uint8_t D3, uint8_t D4, 
+            uint8_t D5, uint8_t D6, uint8_t D7, uint8_t CS1, uint8_t CS2);
 
-  public:
-      int debug = 0;
-      struct Coordinate lcdCoord;
+    public:
+        int debug = 0;
+        struct Coordinates lcdCoord;
+        // struct Virtual_Coordinates virtualCoord;
 
-      GLCD_KS0108(uint8_t DI, uint8_t RW, uint8_t EN, 
-          uint8_t D0, uint8_t D1, uint8_t D2, uint8_t D3, uint8_t D4, 
-          uint8_t D5, uint8_t D6, uint8_t D7, uint8_t CS1, uint8_t CS2);
-      
-      // GLCD_KS0108(uint8_t DI, uint8_t RW, uint8_t EN, uint8_t D4, 
-      //     uint8_t D5, uint8_t D6, uint8_t D7, uint8_t CS1, uint8_t CS2);
+        GLCD_KS0108(uint8_t DI, uint8_t RW, uint8_t EN, 
+            uint8_t D0, uint8_t D1, uint8_t D2, uint8_t D3, uint8_t D4, 
+            uint8_t D5, uint8_t D6, uint8_t D7, uint8_t CS1, uint8_t CS2);
+        
+        // GLCD_KS0108(uint8_t DI, uint8_t RW, uint8_t EN, uint8_t D4, 
+        //     uint8_t D5, uint8_t D6, uint8_t D7, uint8_t CS1, uint8_t CS2);
 
-      void begin(enum Colors color);
-      void gotoXY(uint8_t x, uint8_t y);
-      void clearDisplay(uint8_t color);
-      void setDisplayColor(enum Colors color);
-      void setPixel(uint8_t x, uint8_t y);
-      void printNumber(uint8_t num);
-      void printCharacter(const char* str);
-      uint8_t read_byte(void);
-      void drawHorizontalLine(uint8_t x, uint8_t y, uint8_t length);
-      void drawVerticallyLine(uint8_t x, uint8_t y, uint8_t length);
-      void drawRectangle(uint8_t x, uint8_t y, uint8_t width, uint8_t height);
-      void drawCircle(uint8_t x, uint8_t y, uint8_t radius);
+        void begin(enum Colors color);
+        void gotoXY(uint8_t x, uint8_t y);
+        void clearDisplay(uint8_t color);
+        void setDisplayColor(enum Colors color);
+        void setPixel(uint8_t x, uint8_t y);
+        void printNumber(uint8_t num);
+        void printCharacter(const char* str);
+        uint8_t read_byte(void);
+        void drawHorizontalLine(uint8_t x, uint8_t y, uint8_t length);
+        void drawVerticallyLine(uint8_t x, uint8_t y, uint8_t length);
+        void drawRectangle(uint8_t x, uint8_t y, uint8_t width, uint8_t height);
+        void drawCircle(uint8_t x, uint8_t y, uint8_t radius);
 
-      void drawRoundedRect(uint8_t x, uint8_t y, uint8_t width, uint8_t height, 
-          uint8_t radius1);
+        void drawRoundedRect(uint8_t x, uint8_t y, uint8_t width, uint8_t height, 
+            uint8_t radius1);
 
-      void drawRoundedRect(uint8_t x, uint8_t y, uint8_t width, uint8_t height, 
-          uint8_t radius1, uint8_t radius2);
+        void drawRoundedRect(uint8_t x, uint8_t y, uint8_t width, uint8_t height, 
+            uint8_t radius1, uint8_t radius2);
 
-      void drawRoundedRect(uint8_t x, uint8_t y, uint8_t width, uint8_t height, 
-          uint8_t radius1, uint8_t radius2, uint8_t radius3);
+        void drawRoundedRect(uint8_t x, uint8_t y, uint8_t width, uint8_t height, 
+            uint8_t radius1, uint8_t radius2, uint8_t radius3);
 
-      void drawRoundedRect(uint8_t x, uint8_t y, uint8_t width, uint8_t height, 
-          uint8_t radius1, uint8_t radius2, uint8_t radius3, uint8_t radius4);
+        void drawRoundedRect(uint8_t x, uint8_t y, uint8_t width, uint8_t height, 
+            uint8_t radius1, uint8_t radius2, uint8_t radius3, uint8_t radius4);
+
+        GLCD_KS0108& setDispDirection(enum Directions dir);
 
 };
